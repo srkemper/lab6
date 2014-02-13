316990c679a2af3c60d14b687338bf89d4248919
srkemper <srkemper@stanford.edu>
2014-02-12 23:10:44 -0800

    ajax lab - before stretch goal

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	var url = "/project/" + idNumber;
+	console.log(url);
+	$.get(url, addProject);
+}
+
+function addProject(result) {
+	console.log(result);
+	var selector = "#project" + result['id'] + " .details";
+
+	var details = '<img src="' + result['image'] + '"class="img detailsImage">' + '<h3>' + result['date'] + '</h3>' + result['summary'];
+
+	$(selector).html(details);
 }
 
 /*
@@ -35,4 +47,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', getRandomColor);
+}
+
+function getRandomColor(result) {
+	var colors = result['colors']['hex'];
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
